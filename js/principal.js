1f60327226e103a363ab090ed6c03050328c5bae
Adryan <adryan.campos@escola.pr.gov.br>
2025-10-03 11:07:31 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -2,3 +2,10 @@
     console.log(titulo);
     console.log(titulo.textContent);
     titulo.textContent=("plk nutricao");
+
+
+    var paciente = document.querySelector("#primeiro-paciente");
+    var tdPeso = paciente.querySelector(".info-peso")
+    var peso = tdPeso.textContent;
+    console.log(paciente); //tr
+    console.log(tdPeso); //td que tem peso
