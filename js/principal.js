12bc5931a26906b7364d8005607d9b9eedc145ff
Adryan <adryan.campos@escola.pr.gov.br>
2025-10-03 10:03:05 -0300

    v

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,4 @@
         var titulo = document.querySelector("h1");
         console.log(titulo);
         console.log(titulo.textContent);
-        titulo>textContent=("plk lindo");
+        titulo>textContent = ("plk lindo");
