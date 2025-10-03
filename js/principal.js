ffdf67472dd13164f9b8c1af00e5f425e586c279
Adryan <adryan.campos@escola.pr.gov.br>
2025-10-03 11:10:01 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -5,7 +5,7 @@
 
 
     var paciente = document.querySelector("#primeiro-paciente");
-    var tdPeso = paciente.querySelector(".info-peso")
+    var tdPeso = document.querySelector(".info-peso")
     var peso = tdPeso.textContent;
     console.log(paciente); 
     console.log(peso); 
