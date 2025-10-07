af6b52c54e70299003c5f055553771a277f8f878
Adryan <adryan.campos@escola.pr.gov.br>
2025-10-07 09:17:17 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -1,14 +1,14 @@
    var titulo = document.querySelector("h1");
     console.log(titulo);
     console.log(titulo.textContent);
-    titulo.textContent=("plk nutricao");
+    titulo.textContent = ("plk nutricao");
 
 
     var paciente = document.querySelector("#primeiro-paciente");
     var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
     console.log(paciente); 
-    console.log(tdpeso); 
+    console.log(tdPeso); 
 
 
     var tdAltura = document.querySelector("#primeiro-paciente");
