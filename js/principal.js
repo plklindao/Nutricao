81e731f9bccc1b9993a6c39d21f5b7f58434c4c8
Adryan <adryan.campos@escola.pr.gov.br>
2025-10-07 08:58:57 -0300

    d

--- js/principal.js
+++ js/principal.js
@@ -9,3 +9,7 @@
     var peso = tdPeso.textContent;
     console.log(paciente); 
     console.log(peso); 
+
+    var tdAltura = document.querySelector(".info-altura")
+    var  altura = tdAltura.textContent;
+    console.log(altura);
