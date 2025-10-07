98b730995552f16d461d93dd7b82aac7033ad84f
Adryan <adryan.campos@escola.pr.gov.br>
2025-10-07 09:56:23 -0300

    ~

--- js/principal.js
+++ js/principal.js
@@ -5,6 +5,11 @@ console.log(titulo.textContent);
 titulo.textContent = ("Nego nutritations");
 
 var paciente = document.querySelector("#primeiro-paciente");
+var tdNome = document.querySelector(.info-nome)
+var nome = tdNome.textContent;
+console.log(nome);
+
+
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 console.log(paciente);
@@ -14,3 +19,7 @@ var tdAltura = document.querySelector("#primeiro-paciente");
 var altura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
+
+var imc = peso / (altura * altura);
+tdImc.textContent = imc;
+console.log(imc);
