c5d384a7c277ada4cfeffc296d3fec9447cf6421
Adryan <adryan.campos@escola.pr.gov.br>
2025-10-21 09:57:56 -0300

    6

--- js/principal.js
+++ js/principal.js
@@ -4,24 +4,21 @@ console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent = ("Plk nutricao");
 
-var pacientes = document.querySelectorALL(".paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
-    console.log(pacientes[i]);
+
+    var paciente = pacientes[i];
     var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
-
 
     var tdAltura = document.querySelector(".info-altura");
     var altura = tdAltura.textContent;
 
-
     var tdImc = document.querySelector(".info-imc");
-
 
     var pesoEhvalido = true;
     var alturaEhvalido = true;
-
 
     if (peso <= 0 || peso >= 1000) {
         console.log("Peso inválido!");
