b1233f46aa36d0d7f219b66b941a41170620a782
Adryan <adryan.campos@escola.pr.gov.br>
2025-10-24 10:21:49 -0300

    s

--- js/principal.js
+++ js/principal.js
@@ -2,49 +2,6 @@
 var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = ("Plk nutrição");
-
-var pacientes = document.querySelectorAll(".paciente");
-
-for(var i = 0; i < pacientes.length; i++) {
-
-var paciente = pacientes[i];
-
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
-var tdImc = paciente.querySelector(".info-imc");
-
-var pesoEhValido = true;
-var alturaEhValido = true;
-
-if (peso <= 0 || peso >= 1000){
-console.log("Peso inválido!");
-pesoEhvalido = false;
-tdImc.textContent = "Peso inválido!";
-}
-
-if (altura <= 0 || altura >= 3.00){
-console.log("Altura inválida!");
-alturaEhValido = false;
-tdImc.textContent = "Altura inválida!";
-}
-
-if (alturaEhValido && pesoEhValido){
-var imc = peso / (altura * altura);
-tdImc.textContent = "Inválido!";
-}
-
-else (alturaEhValido && pesoEhValido)
-tdImc.textContent = imc;
-
-}//console.log(document.querySelector("h1"));
-var titulo = document.querySelector("h1");
-console.log(titulo);
-console.log(titulo.textContent);
 titulo.textContent = ("Nego nutritations");
 
 var pacientes = document.querySelectorAll(".paciente");
@@ -68,20 +25,17 @@ if (peso <= 0 || peso >= 1000){
 console.log("Peso inválido!");
 pesoEhvalido = false;
 tdImc.textContent = "Peso inválido!";
-paciente.style.backgrondColor = "lightcoral";
+paciente.style.backgroundColor = "lightcoral";
 }
 
 if (altura <= 0 || altura >= 3.00){
 console.log("Altura inválida!");
 alturaEhValido = false;
 tdImc.textContent = "Altura inválida!";
-paciente.style.backgrondColor = "lightcoral";
 }
 
 if (alturaEhValido && pesoEhValido){
 var imc = peso / (altura * altura);
 tdImc.textContent = imc.toFixed(2);
 }
-
-
 }
