15916a62e91352cdd2ab8a2ac234baa51a882114
Adryan <adryan.campos@escola.pr.gov.br>
2025-10-31 10:04:39 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
 //console.log(document.querySelector("h1"));
 var titulo = document.querySelector("h1");
 console.log(titulo);
@@ -51,6 +53,31 @@ botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
     //alert("OI eu sou o botão e fui clicado");
     var form = document.querySelector
-    });
+});
+
+
+var pacienteTr = document.createElement("tr");
+
+var nomeTd = document.createElement("td");
+var pesoTd = document.createElement("td");
+var alturaTd = document.createElement("td");
+var gorduraTd = document.createElement("td");
+var imcTd = document.createElement("td");
+
+var pacienteTr = document.createElement("tr");
+
+nomeTd.textContent = nome;
+pesoTd.textContent = peso;
+alturaTd.textContent = altura;
+gorduraTd.textContent = gordura;
+
+pacienteTr.appendChild(nomeTd);
+pacienteTr.appendChild(pesoTd);
+pacienteTr.appendChild(alturaTd);
+pacienteTr.appendChild(gorduraTd);
+
+
+var tabela = document.querySelector("#tabela-pacientes");
+tabela.appendChild(pacienteTr);
 
 
